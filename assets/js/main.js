3e64945eff6f607b9052a249d3db0f1985a7f9c4
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-21 17:45:44 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,62 @@
-for (let i = 1; i<=100; i++) {
+
+let numberBox = document.getElementById("container");
+
+let singleBox = document.createElement("div");
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+
 
     if (i % 3 === 0 && i % 5 === 0) {
- 
-     console.log ('FizzBuzz')
-    
+
+        console.log('FizzBuzz')
+        //  document.write("<div>FizzBuzz</div>")
+        let box = document.createElement("div");
+        box.classList.add('colore1');
+        box.innerText = 'Fizzbuzz';
+        numberBox.append(box);
+
+
     }
- 
-    else if (i % 3 === 0){
-     console.log ('Fizz')
+
+    else if (i % 3 === 0) {
+        console.log('Fizz')
+        //  document.write("<div>Fizz</div>");
+        let box = document.createElement("div");
+        box.classList.add('colore2');
+        box.innerText = 'Fizz';
+        numberBox.append(box);
+
+
+
+
     }
- 
-    else if (i % 5 === 0 ) {
-     console.log ('Buzz')
+
+    else if (i % 5 === 0) {
+        console.log('Buzz')
+        //  document.write("<div>Buzz</div>");
+        let box = document.createElement("div");
+        box.classList.add('colore3');
+        box.innerText = 'Buzz';
+        numberBox.append(box);
+
     }
- 
+
     else {
-     console.log (i)
+        console.log(i)
+        //  document.write(`<div>${i}</div> `)
+
+        let box = document.createElement("div");
+        box.classList.add('colore4');
+        box.innerText = `${i}`;
+        numberBox.append(box);
+
+
+
+
+
+
     }
- }
+}
